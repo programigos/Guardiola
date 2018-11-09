9941f37224377da3c9f7bab43f0ffc6195ba37a4
programigos <programigos5@gmail.com>
2018-11-09 12:57:37 -0500

    Líneas comentadas

--- src/pages/balance/balance.ts
+++ src/pages/balance/balance.ts
@@ -44,7 +44,7 @@ export class BalancePage {
     };
     this.information.push(year);
   }
-  ionViewWillEnter(){
+  ionViewWillEnter(){//Volver a la pantalla luego de cambiar
     this.dayForm=this.createDayForm();
     this.monthForm=this.createMonthForm();
     this.yearForm=this.createYearForm();
@@ -84,15 +84,15 @@ export class BalancePage {
     });
   }
 
-  changeDay($event){
+  changeDay($event){//Cambiar el día de la búsqueda por días
     console.log($event);
     let user_data = JSON.parse(localStorage.getItem('usuario_data'));
-    this.saves.getUserDay(user_data.id,$event).then((result)=>{
+    this.saves.getUserDay(user_data.id,$event).then((result)=>{//Cargar resumen por categoría
       let balance = JSON.parse(JSON.stringify(result));
       this.information[0].categorias=balance;
       for(var i=0;i<this.information[0].categorias.length;++i){
         let categoria=this.information[0].categorias[i];
-        this.saves.getUserDayCategory(user_data.id,$event,categoria.id).then((result)=>{
+        this.saves.getUserDayCategory(user_data.id,$event,categoria.id).then((result)=>{//Cargar ingresos/egresos por categoría
           let incomes = JSON.parse(JSON.stringify(result));
           categoria.incomes=incomes;
           console.log("Ingresos:");
@@ -113,15 +113,15 @@ export class BalancePage {
     });
   }
 
-  changeMonth($event){
+  changeMonth($event){//Cambiar el mes de la búsqueda por meses
     console.log($event);
     let user_data = JSON.parse(localStorage.getItem('usuario_data'));
-    this.saves.getUserMonth(user_data.id,$event).then((result)=>{
+    this.saves.getUserMonth(user_data.id,$event).then((result)=>{//Cargar resumen por categoría
       let balance = JSON.parse(JSON.stringify(result));
       this.information[1].categorias=balance;
       for(var i=0;i<this.information[1].categorias.length;++i){
         let categoria=this.information[1].categorias[i];
-        this.saves.getUserMonthCategory(user_data.id,$event,categoria.id).then((result)=>{
+        this.saves.getUserMonthCategory(user_data.id,$event,categoria.id).then((result)=>{//Cargar ingresos/egresos por categoría
           let incomes = JSON.parse(JSON.stringify(result));
           categoria.incomes=incomes;
           console.log("Ingresos:");
@@ -142,15 +142,15 @@ export class BalancePage {
     });
   }
 
-  changeYear($event){
+  changeYear($event){//Cambiar el año de la búsqueda por año
     console.log($event);
     let user_data = JSON.parse(localStorage.getItem('usuario_data'));
-    this.saves.getUserYear(user_data.id,$event).then((result)=>{
+    this.saves.getUserYear(user_data.id,$event).then((result)=>{//Cargar resumen por categoría
       let balance = JSON.parse(JSON.stringify(result));
       this.information[2].categorias=balance;
       for(var i=0;i<this.information[2].categorias.length;++i){
         let categoria=this.information[2].categorias[i];
-        this.saves.getUserYearCategory(user_data.id,$event,categoria.id).then((result)=>{
+        this.saves.getUserYearCategory(user_data.id,$event,categoria.id).then((result)=>{//Cargar ingresos/egresos por categoría
           let incomes = JSON.parse(JSON.stringify(result));
           categoria.incomes=incomes;
           console.log("Ingresos:");
@@ -171,7 +171,7 @@ export class BalancePage {
     });
   }
 
-  toggleSection(idx) {
+  toggleSection(idx) {//Cambiar el rango de fecha
     for(let i=0;i<3;++i){
       if(i==idx)
         this.information[i].open = !this.information[i].open;
@@ -180,7 +180,7 @@ export class BalancePage {
     }
   }
 
-  toggleItem(i, j) {
+  toggleItem(i, j) {//Mostrar ingresos/egresos de una categoría
     this.information[i].categorias[j].open = !this.information[i].categorias[j].open;
   }
 
